c26c4233aa52ee53dc471a04b89786902fde9e71
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-09-15 19:58:53 +0300

    First working vers

---
+++ build/CMakeFiles/AudioPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AudioPlayer"
+  "AudioPlayer.pdb"
+  "CMakeFiles/AudioPlayer.dir/main.cpp.o"
+  "CMakeFiles/AudioPlayer.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AudioPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
