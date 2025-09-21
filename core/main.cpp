97e242db76bb94ba8f28496c88b1518d4f05a08a
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-09-21 13:42:45 +0300

    Added the ability to play all tracks from a dir

--- core/main.cpp
+++ core/main.cpp
@@ -1,9 +1,28 @@
 #include "PlayAudio.h"
 #include <iostream>
+#include <deque>
+#include <filesystem>
+#include <algorithm>
 int main() {
-    const char* path = "/Applications/AudioPlayer/testingmp3mus/ANNA_ASTI_-_Po_baram_74376135.mp3";
+    std::deque<std::filesystem::path> paths_to_files;
+    const char* path_to_dir = "../testingmp3mus";
+    for (auto const& file: std::filesystem::directory_iterator(path_to_dir)) {
+        auto filename = file.path().filename().string();
+        if (!filename.empty() && filename[0] == '.') {
+            continue;
+        }
+        paths_to_files.push_back(file.path());
+    }
+    std::sort(paths_to_files.begin(), paths_to_files.end(), [](const std::filesystem::path& a, const std::filesystem::path& b) {
+        return std::filesystem::last_write_time(a) < std::filesystem::last_write_time(b);
+    });
+    paths_to_files.pop_front();
     try {
-        PlayAudio(path);
+        while (!paths_to_files.empty()) {
+            const char* path = paths_to_files.front().c_str();
+            PlayAudio(path);
+            paths_to_files.pop_front();
+        }
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
