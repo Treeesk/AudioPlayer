70fe02a53703bff7c04815528e61681d40907cf1
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-10-16 10:38:03 +0300

    Add PrevTrack, NextTrack buttons. Create RoundPushButton class

--- HeaderFiles/TrackInfoUI.h
+++ HeaderFiles/TrackInfoUI.h
@@ -2,12 +2,21 @@
 #define TRACKINFOUI_H
 #include <QPushButton>
 
+class RoundPushButton: public QPushButton {
+    Q_OBJECT
+public:
+    RoundPushButton(QWidget *parent = nullptr): QPushButton(parent) {}
+protected:
+    bool hitButton(const QPoint& pos) const;
+    void paintEvent(QPaintEvent* event);
+};
+
 class PlayerControlsWidget: public QWidget{
     Q_OBJECT
 private:
-    QPushButton *PlayStopTrack;
-    QPushButton *PrevTrack;
-    QPushButton *NextTrack;
+    RoundPushButton *PlayStopTrack;
+    RoundPushButton *PrevTrack;
+    RoundPushButton *NextTrack;
 public:
     PlayerControlsWidget(QWidget *parent = nullptr);
 };
--- SourceFiles/TrackInfoUI.cpp
+++ SourceFiles/TrackInfoUI.cpp
@@ -1,15 +1,29 @@
 #include "TrackInfoUI.h"
 #include <QHBoxLayout>
-
+#include <QPainter>
+#include <QPainterPath>
+#include <QDebug>
 
 PlayerControlsWidget::PlayerControlsWidget(QWidget* parent): QWidget(parent) {
     QIcon playIcon(":/Resources/Icons/Play.png");
     QIcon stopIcon(":/Resources/Icons/Stop.png");
-    PlayStopTrack = new QPushButton(this);
+    QIcon nexttrackIcon(":/Resources/Icons/Next.png");
+    QIcon prevtrackIcon(":/Resources/Icons/Prev.png");
+    PlayStopTrack = new RoundPushButton(this);
+    NextTrack = new RoundPushButton(this);
+    PrevTrack = new RoundPushButton(this);
+    NextTrack->setIcon(nexttrackIcon);
+    NextTrack->setIconSize(QSize(35, 35)); // тест
+    NextTrack->setFixedSize(50, 50); // тест
+
+    PrevTrack->setIconSize(QSize(35, 35)); // тест
+    PrevTrack->setFixedSize(50, 50); // тест
+
+    PrevTrack->setIcon(prevtrackIcon);
     PlayStopTrack->setCheckable(true);
     PlayStopTrack->setIcon(playIcon);
-    PlayStopTrack->setIconSize(QSize(35, 35));
-    PlayStopTrack->setFixedSize(50, 50);
+    PlayStopTrack->setIconSize(QSize(35, 35)); // тест
+    PlayStopTrack->setFixedSize(50, 50); // тест
     connect(PlayStopTrack, &QPushButton::toggled, this, [this, playIcon, stopIcon](bool checked) {
         if (!checked) {
             this->PlayStopTrack->setIcon(playIcon);
@@ -20,4 +34,27 @@ PlayerControlsWidget::PlayerControlsWidget(QWidget* parent): QWidget(parent) {
     });
     QHBoxLayout* layout = new QHBoxLayout(this); // лэйаут подключаем к виджету, и добавляем кнопки виджета к лэйауту
     layout->addWidget(PlayStopTrack);
+    layout->addWidget(NextTrack);
+    layout->addWidget(PrevTrack);
+}
+
+bool RoundPushButton::hitButton(const QPoint& pos) const {
+    QSize iconsize = this->iconSize();
+    double radius = iconsize.width() / 2.0;
+    double dist_from_center_x = pos.x() - width() / 2.0;
+    double dist_from_center_y = pos.y() - height() / 2.0;
+    return (dist_from_center_x * dist_from_center_x + dist_from_center_y * dist_from_center_y) <= (radius * radius);
+}
+
+void RoundPushButton::paintEvent(QPaintEvent* event) {
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing); // сглаживание краев
+    QPainterPath path;
+    QPixmap img = this->icon().pixmap(this->iconSize());
+    QPointF topLeft((width() - this->iconSize().width()) / 2.0, (height() - this->iconSize().height()) / 2.0);
+    path.addEllipse(QRectF(topLeft, this->iconSize()));
+    painter.setClipPath(path);
+    painter.drawPixmap(topLeft, img);
+    painter.setPen(Qt::NoPen);
+    painter.drawPath(path);
 }
